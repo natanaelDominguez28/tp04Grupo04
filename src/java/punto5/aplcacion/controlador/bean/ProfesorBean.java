f9d2d7ea61f75203ed84f91b5f352800707e1c4b
Lore <Lore@DESKTOP-CGP1K6U>
2018-04-27 22:35:17 -0300

    agregando pto5 tp4 Profesor

---
+++ src/java/punto5/aplcacion/controlador/bean/ProfesorBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto5.aplcacion.controlador.bean;
+
+import punto5.aplicacion.modelo.dominio.Profesor;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Lore
+ */
+@ManagedBean
+@ViewScoped
+public class ProfesorBean implements Serializable{
+    private Profesor profesor;
+    
+
+    /**
+     * Creates a new instance of ProfesorBean
+     */
+    public ProfesorBean() {
+        profesor=new Profesor ();
+    }
+    
+
+    /**
+     * @return the profesor
+     */
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    /**
+     * @param profesor the profesor to set
+     */
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+    
+}
+
---
+++ src/java/punto5/aplicacion/modelo/dominio/Profesor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto5.aplicacion.modelo.dominio;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Lore
+ */
+public class Profesor implements Serializable{
+
+    private String legajo;
+    private String nombre;
+    private String apellido;
+    
+    
+    public Profesor() {
+    }
+
+    /**
+     * @return the legajo
+     */
+    public String getLegajo() {
+        return legajo;
+    }
+
+    /**
+     * @param legajo the legajo to set
+     */
+    public void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
+
